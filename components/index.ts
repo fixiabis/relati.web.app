5dce20b060745e1eb11038d57f0319f270e04825
fixiabis <fixiabis@gmail.com>
2020-12-27 13:02:23 +0800

    feat: export components

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+export { default as Button, ButtonGroup } from './Button';
+export { default as Dialog } from './Dialog';
+export { default as LinkButton } from './LinkButton';
+export { default as Logo } from './Logo';
+export { default as Popup } from './Popup';
+
+export type { ButtonProps, ButtonGroupProps } from './Button';
+export type { DialogProps } from './Dialog';
+export type { LinkButtonProps } from './LinkButton';
+export type { LogoProps } from './Logo';
+export type { PopupProps } from './Popup';
