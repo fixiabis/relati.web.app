6f68e1f26619423ab9ce28c4bac08e2a462453ef
fixiabis <fixiabis@gmail.com>
2020-12-29 20:33:46 +0800

    refactor(game): name Component

--- components/page/game/layouts/GameFor1PLayoutBuilder.tsx
+++ components/page/game/layouts/GameFor1PLayoutBuilder.tsx
@@ -24,128 +24,137 @@ const playersCount = PLAYERS_COUNT_FOR_2P;
 
 const GameFor1PLayoutBuilder: (player: Player) => GameLayoutComponent = (
   player
-) => ({ boardWidth, boardHeight, portsCount, onLeave: emitLeave }) => {
-  const definition = useMemo(
-    () =>
-      GameDefinition<Player, Piece>(
-        playersCount,
-        boardWidth,
-        boardHeight,
-        DirectionRoutes['P' + portsCount]
-      ),
-    [boardWidth, boardHeight, portsCount]
-  );
-
-  const thinker = useMemo(() => {
-    const multiInfluencesBasedThinking = MultiInfluencesBasedThinking(
-      definition
+) => {
+  const GameFor1PLayout: GameLayoutComponent = ({
+    boardWidth,
+    boardHeight,
+    portsCount,
+    onLeave: emitLeave,
+  }) => {
+    const definition = useMemo(
+      () =>
+        GameDefinition<Player, Piece>(
+          playersCount,
+          boardWidth,
+          boardHeight,
+          DirectionRoutes['P' + portsCount]
+        ),
+      [boardWidth, boardHeight, portsCount]
     );
 
-    const deepThinking = DeepThinking(multiInfluencesBasedThinking, 1);
+    const thinker = useMemo(() => {
+      const multiInfluencesBasedThinking = MultiInfluencesBasedThinking(
+        definition
+      );
 
-    const thinker = Thinker(deepThinking);
+      const deepThinking = DeepThinking(multiInfluencesBasedThinking, 1);
 
-    return thinker;
-  }, [definition]);
+      const thinker = Thinker(deepThinking);
 
-  const [game, setGame] = useState(() => Game(GameRule(definition)));
+      return thinker;
+    }, [definition]);
 
-  const [pieceIndexesOfPlacement, setPieceIndexesOfPlacement] = useState<
-    number[]
-  >([]);
+    const [game, setGame] = useState(() => Game(GameRule(definition)));
 
-  const [
-    isGameOverDialogVisible,
-    openGameOverDialog,
-    closeGameOverDialog,
-  ] = useDialogState(false);
+    const [pieceIndexesOfPlacement, setPieceIndexesOfPlacement] = useState<
+      number[]
+    >([]);
 
-  const [
-    isGameLeaveDialogVisible,
-    openGameLeaveDialog,
-    closeGameLeaveDialog,
-  ] = useDialogState(false);
+    const [
+      isGameOverDialogVisible,
+      openGameOverDialog,
+      closeGameOverDialog,
+    ] = useDialogState(false);
 
-  const [
-    isGameRetryDialogVisible,
-    openGameRetryDialog,
-    closeGameRetryDialog,
-  ] = useDialogState(false);
+    const [
+      isGameLeaveDialogVisible,
+      openGameLeaveDialog,
+      closeGameLeaveDialog,
+    ] = useDialogState(false);
 
-  useEffect(() => {
-    const playerOfTurn = (game.turn % playersCount) as Player;
+    const [
+      isGameRetryDialogVisible,
+      openGameRetryDialog,
+      closeGameRetryDialog,
+    ] = useDialogState(false);
 
-    if (playerOfTurn !== player) {
-      setTimeout(() => {
-        const pieceIndex = thinker.getPieceIndexForPlacement(game);
-        const gameWithPiecePlaced = game.place(pieceIndex, playerOfTurn);
+    useEffect(() => {
+      const playerOfTurn = (game.turn % playersCount) as Player;
+
+      if (playerOfTurn !== player) {
+        setTimeout(() => {
+          const pieceIndex = thinker.getPieceIndexForPlacement(game);
+          const gameWithPiecePlaced = game.place(pieceIndex, playerOfTurn);
+          setGame(gameWithPiecePlaced);
+          setPieceIndexesOfPlacement([...pieceIndexesOfPlacement, pieceIndex]);
+        }, 500);
+      }
+    }, [game]);
+
+    const winner = game.rule.getWinner(game);
+    const canControlDirectly = winner !== NO_WINNER || game.turn === 0;
+
+    const resetGame = () => {
+      setGame(Game(game.rule));
+      closeGameRetryDialog();
+    };
+
+    const handleRetry = canControlDirectly ? resetGame : openGameRetryDialog;
+    const handleLeave = canControlDirectly ? emitLeave : openGameLeaveDialog;
+
+    const handleGridClick: BoardFor2PGameProps['onGridClick'] = ({ x, y }) => {
+      const pieceIndex = definition.toPieceIndex([x, y]);
+      const gameWithPiecePlaced = game.place(pieceIndex, player);
+
+      if (gameWithPiecePlaced !== game) {
         setGame(gameWithPiecePlaced);
         setPieceIndexesOfPlacement([...pieceIndexesOfPlacement, pieceIndex]);
-      }, 500);
-    }
-  }, [game]);
+      }
 
-  const winner = game.rule.getWinner(game);
-  const canControlDirectly = winner !== NO_WINNER || game.turn === 0;
+      openGameOverDialog();
+    };
 
-  const resetGame = () => {
-    setGame(Game(game.rule));
-    closeGameRetryDialog();
+    return (
+      <Container>
+        <BoardFor2PGame
+          game={game}
+          pieceIndexesOfPlacement={pieceIndexesOfPlacement}
+          onGridClick={handleGridClick}
+        />
+
+        <Button.DenceGroup className="bottom-right-fixed">
+          <Button title="重開" color="#888" onClick={handleRetry}>
+            <Icon url={LightRetryIconUrl} />
+          </Button>
+          <Button title="離開" color="#888" onClick={handleLeave}>
+            <Icon url={LightLeaveIconUrl} />
+          </Button>
+        </Button.DenceGroup>
+
+        <GameOverDialog
+          visible={isGameOverDialogVisible}
+          onClose={closeGameOverDialog}
+          winner={winner}
+          onRetry={resetGame}
+          onLeave={emitLeave}
+        />
+
+        <GameLeaveDialog
+          visible={isGameLeaveDialogVisible}
+          onClose={closeGameLeaveDialog}
+          onLeave={emitLeave}
+        />
+
+        <GameRetryDialog
+          visible={isGameRetryDialogVisible}
+          onClose={closeGameRetryDialog}
+          onRetry={resetGame}
+        />
+      </Container>
+    );
   };
 
-  const handleRetry = canControlDirectly ? resetGame : openGameRetryDialog;
-  const handleLeave = canControlDirectly ? emitLeave : openGameLeaveDialog;
-
-  const handleGridClick: BoardFor2PGameProps['onGridClick'] = ({ x, y }) => {
-    const pieceIndex = definition.toPieceIndex([x, y]);
-    const gameWithPiecePlaced = game.place(pieceIndex, player);
-
-    if (gameWithPiecePlaced !== game) {
-      setGame(gameWithPiecePlaced);
-      setPieceIndexesOfPlacement([...pieceIndexesOfPlacement, pieceIndex]);
-    }
-
-    openGameOverDialog();
-  };
-
-  return (
-    <Container>
-      <BoardFor2PGame
-        game={game}
-        pieceIndexesOfPlacement={pieceIndexesOfPlacement}
-        onGridClick={handleGridClick}
-      />
-
-      <Button.DenceGroup className="bottom-right-fixed">
-        <Button title="重開" color="#888" onClick={handleRetry}>
-          <Icon url={LightRetryIconUrl} />
-        </Button>
-        <Button title="離開" color="#888" onClick={handleLeave}>
-          <Icon url={LightLeaveIconUrl} />
-        </Button>
-      </Button.DenceGroup>
-
-      <GameOverDialog
-        visible={isGameOverDialogVisible}
-        onClose={closeGameOverDialog}
-        winner={winner}
-        onRetry={resetGame}
-        onLeave={emitLeave}
-      />
-
-      <GameLeaveDialog
-        visible={isGameLeaveDialogVisible}
-        onClose={closeGameLeaveDialog}
-        onLeave={emitLeave}
-      />
-
-      <GameRetryDialog
-        visible={isGameRetryDialogVisible}
-        onClose={closeGameRetryDialog}
-        onRetry={resetGame}
-      />
-    </Container>
-  );
+  return GameFor1PLayout;
 };
 
 export default GameFor1PLayoutBuilder;
