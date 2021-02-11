d4f1261d3d772e1132c5c2cfc13e74c11dc073b6
fixiabis <fixiabis@gmail.com>
2021-02-11 11:58:19 +0800

    feat: add button effect

--- components/layouts/choose/tutorial/stage.tsx
+++ components/layouts/choose/tutorial/stage.tsx
@@ -1,33 +1,38 @@
 import { Button } from '../../../core';
-import { FadeInLinkButton } from '../../../shared';
+import { SlideLeftFadeInLinkButton } from '../../../shared';
 
 export const renderButtons = (boardParams: string) => (
   <Button.Group>
-    <FadeInLinkButton
+    <SlideLeftFadeInLinkButton
+      animationDelay="0.25s"
       href={`/tutorial?board=${boardParams}&stage=1`}
       title="1"
       backgroundColor="crimson"
       children="1"
     />
-    <FadeInLinkButton
+    <SlideLeftFadeInLinkButton
+      animationDelay="0.5s"
       href={`/tutorial?board=${boardParams}&stage=2`}
       title="2"
       backgroundColor="royalblue"
       children="2"
     />
-    <FadeInLinkButton
+    <SlideLeftFadeInLinkButton
+      animationDelay="0.75s"
       href={`/tutorial?board=${boardParams}&stage=3`}
       title="3"
       backgroundColor="darkorange"
       children="3"
     />
-    <FadeInLinkButton
+    <SlideLeftFadeInLinkButton
+      animationDelay="1s"
       href={`/tutorial?board=${boardParams}&stage=4`}
       title="4"
       backgroundColor="seagreen"
       children="4"
     />
-    <FadeInLinkButton
+    <SlideLeftFadeInLinkButton
+      animationDelay="1.25s"
       href={`/tutorial?board=${boardParams}&stage=5`}
       title="5"
       backgroundColor="purple"
