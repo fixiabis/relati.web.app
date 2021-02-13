ca940f9aa9cf058661fe11a78b6133d4a9fdc865
fixiabis <fixiabis@gmail.com>
2021-02-13 20:31:01 +0800

    feat: Icon disable flex shrink

--- components/core/Icon.tsx
+++ components/core/Icon.tsx
@@ -2,8 +2,11 @@ import { styled } from './utils';
 
 export type IconProps = { url: string };
 
-const Icon = styled.div<IconProps>({ width: 50, height: 50 }, ({ url }) => ({
-  backgroundImage: `url(${url})`,
-}));
+const Icon = styled.div<IconProps>(
+  { width: 50, height: 50, flexShrink: 0 },
+  ({ url }) => ({
+    backgroundImage: `url(${url})`,
+  })
+);
 
 export default Icon;
