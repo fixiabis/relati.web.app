8ae49d3794a77eb6b58593af70a3cb3f8dec8c01
fixiabis <fixiabis@gmail.com>
2021-02-13 20:21:06 +0800

    feat: add slideTopFadeIn effect

--- components/core/animations.ts
+++ components/core/animations.ts
@@ -8,6 +8,10 @@ export const fadeIn = keyframes({
   from: { opacity: 0 },
 });
 
+export const slideTopFadeIn = keyframes({
+  from: { opacity: 0, transform: 'translateY(100%)' },
+});
+
 export const slideLeftFadeIn = keyframes({
   from: { opacity: 0, transform: 'translateX(100%)' },
 });
