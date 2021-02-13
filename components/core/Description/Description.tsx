f90e29eb2f40b480ead4d24b7dd3ad21e07d0571
fixiabis <fixiabis@gmail.com>
2021-02-13 20:42:04 +0800

    feat: configurable width

--- components/core/Description/Description.tsx
+++ components/core/Description/Description.tsx
@@ -2,14 +2,19 @@ import styled from '@emotion/styled';
 import * as styles from '../styles';
 import DescriptionText from './DescriptionText';
 
+export type DescriptionProps = { width?: string };
+
 const DescriptionNamespace = {
   Text: DescriptionText,
 };
 
-const Description = styled.div({
-  height: '90px',
-  padding: '10px',
-  ...styles.flexCenter,
-});
+const Description = styled.div<DescriptionProps>(
+  {
+    height: '90px',
+    padding: '10px',
+    ...styles.flexCenter,
+  },
+  ({ width = 'auto' }) => ({ width })
+);
 
 export default Object.assign(Description, DescriptionNamespace);
