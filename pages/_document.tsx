30266e567f115e0863dcacb26c9498e6c7dea734
fixiabis <fixiabis@gmail.com>
2021-02-16 22:18:04 +0800

    feat: add description meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,12 @@ class DocumentRoot extends Document {
   render() {
     return (
       <Html lang="zh-Hant-TW">
-        <Head />
+        <Head>
+          <meta
+            name="description"
+            content="relati是一款基於連線的棋盤遊戲，玩家將需要活用特定的連線路徑去下棋，在沒辦法下棋時，便會輸掉遊戲。"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
